60d6d6c02e036b96479388d10bbca8d4fb6ce5b5
lhd <1056569214@qq.com>
2021-07-17 13:31:11 +0800

    lhppd

--- js/zduan.js
+++ js/zduan.js
@@ -10,9 +10,9 @@ var system = {
 var p = navigator.platform;
 system.win = p.indexOf("Win") == 0;
 system.mac = p.indexOf("Mac") == 0;
-system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+system.xll = (p == "Xll") || (p.indexOf("Linux") == 0);
 //跳转语句 
-if (system.win || system.mac || system.x11) {//转向后台登陆页面 
+if (system.win || system.mac || system.xll) {//转向后台登陆页面 
     window.location.href = "http://www.baidu.com";
 }
 
