9e7047ccd0a62033d3b762548771d834549ebb4a
lhd <1056569214@qq.com>
2021-07-17 13:14:46 +0800

    lhppd

--- js/zduan.js
+++ js/zduan.js
@@ -1,42 +1,20 @@
 
-// //平台、设备和操作系统 
-// var system = {
-//     win: false,
-//     mac: false,
-//     xll: false
-// };
-
-// //检测平台 
-// var p = navigator.platform;
-// system.win = p.indexOf("Win") == 0;
-// system.mac = p.indexOf("Mac") == 0;
-// system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-// //跳转语句 
-// if (system.win || system.mac || system.xll) {//转向后台登陆页面 
-//     window.location.href = "http://www.baidu.com";
-// }
-
-
-function IsPC(){  
-    var userAgentInfo = navigator.userAgent;
-    var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
-    var flag = true;  
-    for (var v = 0; v < Agents.length; v++) {  
-        if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }  
-    }  
-    return flag;  
- }
-
- if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
-    //alert(navigator.userAgent);  
-    window.location.href ="http://crazyl.work";
-} else if (/(Android)/i.test(navigator.userAgent)) {
-    //alert(navigator.userAgent); 
-    window.location.href ="http://crazyl.work";
-} else {
-    window.location.href ="http://www.baidu.com";
+//平台、设备和操作系统 
+var system = {
+    win: false,
+    mac: false,
+    xll: false
 };
 
+//检测平台 
+var p = navigator.platform;
+system.win = p.indexOf("Win") == 0;
+system.mac = p.indexOf("Mac") == 0;
+system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+//跳转语句 
+if (system.win || system.mac || system.x11) {//转向后台登陆页面 
+    window.location.href = "http://www.baidu.com";
+}
 
 document.onkeydown = document.onkeyup = document.onkeypress = function (event) {
     var e = event || window.event || arguments.callee.caller.arguments[0];
