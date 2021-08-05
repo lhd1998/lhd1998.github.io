f49f21015c63b461016a17c7efac8bfdd46bef00
lhd <1056569214@qq.com>
2021-08-05 13:46:44 +0800

    lhppd

--- yx/src/project.js
+++ yx/src/project.js
@@ -1021,9 +1021,9 @@ window.__require = function e(t, n, o) {
           }).union().repeatForever().start()
         }, t.prototype.update = function (e) {
         }, t.prototype.adsButtonFunc2 = function () {
-          window.location.href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzkyOTI3MDg5Ng==#wechat_redirect"
+          window.location.href = "https://mp.weixin.qq.com/s/82Fehk9KH2hUW3F87y_evQ"
         }, t.prototype.bannerButtonFunc = function () {
-          window.location.href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzkyOTI3MDg5Ng==#wechat_redirect"
+          window.location.href = "https://mp.weixin.qq.com/s/82Fehk9KH2hUW3F87y_evQ"
         }, t.prototype.ShowFailedUi = function (e, t) {
           var n = this;
           this.scheduleOnce(function () {
@@ -1983,7 +1983,7 @@ window.__require = function e(t, n, o) {
         }, t.prototype.update = function (e) {
           this.UpdateScoreLabel(e), this.lerpCtrl && this.lerpNumFunc(this.passlevelYQ), this.levelPanel.children[1].getComponent(cc.Label).string = s.default.Instance.GetLevel().toString()
         }, t.prototype.adsButtonFunc = function () {
-          window.location.href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzkyOTI3MDg5Ng==#wechat_redirect";
+          window.location.href = "https://mp.weixin.qq.com/s/82Fehk9KH2hUW3F87y_evQ";
         }, t.prototype.TestPasslevel = function () {
           var e = this;
           this.lerpCtrl = !0, this.nowYQ >= this.passlevelYQ && (this.levelPanel.children[2].runAction(cc.sequence(cc.delayTime(1.3), cc.callFunc(function () {
