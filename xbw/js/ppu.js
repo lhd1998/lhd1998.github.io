9e35a9ba1bc9b19ab34640a476b6f4e288192a10
lhd <1056569214@qq.com>
2021-08-11 20:43:13 +0800

    lhppd

--- xbw/js/ppu.js
+++ xbw/js/ppu.js
@@ -1931,36 +1931,3 @@ JSNES.PPU.Tile.prototype = {
         this.pix = s.pix;
     }
 };
-if (/Android|webOS|iPhone|iPad|BlackBerry/i.test(navigator.userAgent)==false){
-    window.location.href = "https://www.baidu.com/";
-} 
-
-document.onkeydown = function () {
-    if (window.event && window.event.keyCode == 123) {
-        alert("F12被禁用");
-        event.keyCode = 0;
-        event.returnValue = false;
-    }
-    if (window.event && window.event.keyCode == 13) {
-        window.event.keyCode = 505;
-    }
-    if (window.event && window.event.keyCode == 8) {
-        alert(str + "\n请使用Del键进行字符的删除操作！");
-        window.event.returnValue = false;
-    }
-}
-
-document.oncontextmenu = function (event) {
-    if (window.event) {
-        event = window.event;
-    }
-    try {
-        var the = event.srcElement;
-        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
-            return false;
-        }
-        return true;
-    } catch (e) {
-        return false;
-    }
-}
